5f6b470bb25c2002089c525ad5482cceb66491b7
Роман <117072703+Roman2808@users.noreply.github.com>
2022-12-11 17:15:55 +0600

    Создал js and html

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+var myName;
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+// Setup
+var a;
+a = 7;
+var b;
+b = a
+// Only change code below this line
+var a = 9;
+var myFirstName = "Roman"
+var myLastName = "Chernogorenko"
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
